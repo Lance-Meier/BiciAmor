0dfb0cff9daf384d89304a527dcffbbde4d4e742
Lance Meier <lancemeier@gmail.com>
2018-03-19 12:23:33 -0600

    added map markers

--- Client/src/app/components/area/area.component.ts
+++ Client/src/app/components/area/area.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { } from '@types/googlemaps';
 
+const listMarkes = [];
+
 @Component({
   selector: 'app-area',
   templateUrl: './area.component.html',
@@ -18,45 +20,73 @@ export class AreaComponent implements OnInit {
   currentLong: any;
   marker: google.maps.Marker;
   mapProp = {
-    center: new google.maps.LatLng(45.0037, -93.2527),
-    zoom: 15,
+    center: new google.maps.LatLng(40.7831, -73.9712),
+    zoom: 14,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
   constructor(private auth: AuthService, private router: Router) { }
 
-  logout() {
-    this.auth.logout();
-    this.router.navigate(['']);
-  }
-  showPosition(position) {
-    this.currentLat = position.coords.latitude;
-    this.currentLong = position.coords.longitude;
-    const location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    this.map.panTo(location);
-
-
-    if (!this.marker) {
-      this.marker = new google.maps.Marker({
-        position: location,
-        map: this.map,
-        title: 'You are here'
-      });
-    } else {
-      this.marker.setPosition(location);
+    // Retrieve Initial Postion
+    showPosition(position) {
+      this.currentLat = position.coords.latitude;
+      this.currentLong = position.coords.longitude;
+      const location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      this.map.panTo(location);
+      if (!this.marker) {
+        this.marker = new google.maps.Marker({
+          position: location,
+          map: this.map,
+          title: 'You are here'
+        });
+      } else {
+        this.marker.setPosition(location);
+      }
     }
-  }
-
-  findMe() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.showPosition(position);
-      });
-    } else {
-      alert('Geolocation is not supported by this browser.');
+      // Determine User Location and Pass to Map
+    findMe() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          this.showPosition(position);
+        });
+      } else {
+        alert('Geolocation is not supported by this browser.');
+      }
     }
-  }
 
+
+
+      // Drop Bike Shop Markers
+    createMarker(place) {
+        const marker =  new google.maps.Marker({
+          position: new google.maps.LatLng(place.geometry.viewport.f.f, place.geometry.viewport.b.b),
+          map: this.map
+        });
+    }
+
+    findNearby() {
+      const request = {
+        location: {lat: this.currentLat, lng: this.currentLong},
+        radius: 5000,
+        type: 'bicycle_store'
+      };
+      const service = new google.maps.places.PlacesService(this.map);
+      service.nearbySearch(request, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (let i = 0; i < results.length; i++) {
+            console.log(results[i]);
+            this.createMarker(results[i]);
+          }
+        }
+      });
+    }
+
+    // Standard Logout Function
+    logout() {
+      this.auth.logout();
+      this.router.navigate(['']);
+    }
+  // When the page loads, create the prop map, then set the browser position to the current position
   ngOnInit() {
     this.map = new google.maps.Map(this.gmapElement.nativeElement, this.mapProp);
     this.findMe();
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -7,3 +7,105 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+/* multer how to for a single file, muliple files, and multiple objects of multiple files
+
+install project, multer, cors
+
+const multer = require("multer");
+const uploads = multer({dest: 'public/uploads'});
+
+router.post('/single', uploads.single('one'), (req,res)=>{
+  res.json('http://localhost:3000/uploads'+req.file.filename);
+})
+
+router.post('/array', uploads.array('photos'), (req, res)=> {
+  let arrayOfLinks = [];
+  for (let file of req.files) {
+    arrayOfLinks.push('http://localhost:3000/uploads'file.filename);
+  }
+  res.send(arrayOfLinks);
+})
+
+router.post('/multiple',
+  uploads.fields([
+    {name: 'profilePic}, maxCount: 1},
+    {name: 'gallery', maxCount:12}
+  ]),
+  (req,res)=> {
+    let links={};
+    let galleryArray=[];
+    links['profilePic'] = 'http://localhost:3000/uploads'+req.files.profilePic[0].filename;
+    for(let file of req.files.gallery){
+      links['gallery'] = galleryArray.push('http://localhost:3000/uploads'+file.filename);
+    }
+    links['gallery'] =  galeryArray;
+    user.gallery = links.gallery;
+    res.json(links);
+  })
+
+
+  IN ANGULAR
+
+  create a service called fileservice
+  import http, map, catch, observable
+
+  constructor(provate http:Http){}
+
+  singleFile(formData){
+    this.http.post('http://localhost:3000/single', formData)
+    .map(res.json())
+    .catch(e=>Observable.throw(e));
+  }
+}
+
+
+
+
+  app module:
+  import http module
+  import form module
+
+  import fileservice
+
+  draw our form in the html
+  <form #myForm="ngForm" (ngSubmit)="onSubmit(myForm)">
+    <input type="text" placeholder="email" name="email" ngModel/>
+    <input multiple type="file" name="profilePic" (change)="manageFile($event)"/>
+    <input type="submit">
+  </form>
+
+
+
+  In Component
+
+  import file service and 
+  constructor(private fileService: FileService)
+
+  formData = new FormData();
+
+  onSubmit(form){
+    const keys = Object.keys(form.value);
+    for (let key of keys){
+      this.formData.append(key, form.value[key])
+    }
+    this.fileService.singleFile(this.formData)
+    .subscribe(res.console.log(res));
+  }
+
+  manageFile(e){
+    for (let file of e.target.files){
+      this.formData.append(e.target.name, file);
+    }
+  }
+
+  create in service 
+
+  single
+  multiple
+
+
+
+
+
+*/
