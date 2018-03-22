3b8c1fbde205a9d0cad07de4dba2db5862494677
Lance Meier <lancemeier@gmail.com>
2018-03-21 18:35:42 -0600

    updated app.js to fix error

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,13 @@
-cp -r ./client/dist .server/public
-rm -rf ./client/dist
+cd Client
+rm -rf dist
+echo 'Building client...'
+ng build --env --aot=false
+cd ..
+echo "Copy files to public..."
+rm -rf ./server/public
+cp -r ./client/dist ./server/public
+
+git add .
+git commit -m "Building sources commit"
+git subtree push --prefix=Server heroku master
+echo "Done!"
